9bb63f7e958c78ae0707d1d03a64bb997cd415af
Hanumanta Gouda <hanumanthgouda93@gmail.com>
2026-03-06 10:27:59 +0530

    multiple object task

---
+++ Cooker.java
@@ -0,0 +1,31 @@
+class Cooker {
+    String cookerBrand;
+    int cookerId;
+    double cookerPrice;
+    boolean isAvailable;
+    String cookerType;
+    int capacity;
+    double weight;
+    boolean isInductionCompatible;
+    String material;
+    int warrantyYears;
+    double diameter;
+    boolean isDishwasherSafe;
+    String color;
+    int stockQuantity;
+    double thickness;
+    boolean isEcoFriendly;
+    String lidType;
+    int rating;
+    double pressureLevel;
+    boolean isCertified;
+    String countryOfOrigin;
+    int manufacturingYear;
+    double handleLength;
+    boolean isNonStick;
+    String certificationType;
+
+    Cooker() {
+        System.out.println("Cooker constructor is invoked");
+    }
+}
