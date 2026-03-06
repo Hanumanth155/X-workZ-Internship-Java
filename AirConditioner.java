4ea530eea571ce0646ab5f36f03b992c9008bd77
Hanumanta Gouda <hanumanthgouda93@gmail.com>
2026-03-06 12:23:38 +0530

    adding object task

---
+++ AirConditioner.java
@@ -0,0 +1,31 @@
+class AirConditioner {
+    String acBrand;
+    int acId;
+    double acPrice;
+    boolean isAvailable;
+    String acType;
+    double capacityTons;
+    double weight;
+    boolean isInverter;
+    String compressorType;
+    int warrantyYears;
+    double coolingCapacity;
+    boolean hasRemote;
+    String color;
+    int stockQuantity;
+    double powerConsumption;
+    boolean isEcoFriendly;
+    String refrigerantType;
+    int rating;
+    double airFlowRate;
+    boolean isCertified;
+    String countryOfOrigin;
+    int manufacturingYear;
+    double noiseLevel;
+    boolean supportsWifi;
+    String certificationType;
+
+    AirConditioner() {
+        System.out.println("AirConditioner constructor is invoked");
+    }
+}
