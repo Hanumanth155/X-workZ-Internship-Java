ffbc6c9ff4bb0e15793a5828420d2f22ac4c4f35
Hanumanta Gouda <hanumanthgouda93@gmail.com>
2026-03-09 10:34:05 +0530

    array intialization

--- AirConditioner.java
+++ AirConditioner.java
@@ -1,31 +1,11 @@
 class AirConditioner {
-    String acBrand;
-    int acId;
-    double acPrice;
-    boolean isAvailable;
-    String acType;
-    double capacityTons;
-    double weight;
-    boolean isInverter;
-    String compressorType;
-    int warrantyYears;
-    double coolingCapacity;
-    boolean hasRemote;
-    String color;
-    int stockQuantity;
-    double powerConsumption;
-    boolean isEcoFriendly;
-    String refrigerantType;
-    int rating;
-    double airFlowRate;
-    boolean isCertified;
-    String countryOfOrigin;
-    int manufacturingYear;
-    double noiseLevel;
-    boolean supportsWifi;
-    String certificationType;
-
-    AirConditioner() {
-        System.out.println("AirConditioner constructor is invoked");
-    }
+    String brand;
+    String model;
+    double price;
+    String type;
+    int weight;
+    String[] colors;
+    int warranty;
+    boolean working;
 }
+
